83b8547a95f0ac2b769e605d5260560fe6a1ba59
Sina <you@example.com>
2017-12-11 13:55:38 +0100

    Working in Opened-loop with very low frequency. the backed-up files of RAPID are in 14000-500232_BACKUP_2017-11-30-working_YuMI_Pos_Vel_Control.rar

--- yumi_motion_example/src/Main.cpp
+++ yumi_motion_example/src/Main.cpp
@@ -13,12 +13,14 @@ using namespace Eigen;
 
 bool Joint_state_recived=false;
 VectorXd State_handle;
+VectorXd State;
 string Name_joint[14];
 trajectory_msgs::JointTrajectory armCommand;
 trajectory_msgs::JointTrajectoryPoint desiredConfiguration;
 
 float joint_positions_down[JOINT_NUMBER] = {-1.5901633501052856, 2.758988380432129, -0.776835024356842, -0.7566481828689575, -1.2274471521377563, -1.1534804105758667, -3.3565382957458496, -3.377692699432373, 1.941343903541565, 1.8748698234558105, 1.8207812309265137, 1.2813187837600708, -0.5231171250343323, -0.42509302496910095};
 float joint_positions_up[JOINT_NUMBER] = {-0.004894050769507885, 0.01121285930275917, -0.7481237053871155, -0.7881973385810852, 0.39989161491394043, 0.45870086550712585, -1.0020086765289307, -2.4347167015075684, 0.9538304805755615, -0.7194674015045166, 0.5497622489929199, -0.2827472388744354, 0.15741610527038574, 0.007038898766040802};
+float joint_positions_zero[JOINT_NUMBER] = {0, 0,0,0, 0.39989161491394043, 0.45870086550712585, -1.0020086765289307, -2.4347167015075684, 0.9538304805755615, -0.7194674015045166, 0.5497622489929199, -0.2827472388744354, 0.15741610527038574, 0.007038898766040802};
 
 void chatterCallback_state(const sensor_msgs::JointState & msg)
 {
@@ -26,7 +28,11 @@ void chatterCallback_state(const sensor_msgs::JointState & msg)
 	{
 		for (int i=0;i<JOINT_NUMBER;i++)
 		{
-			State_handle(i)=msg.position[i];
+			if (Joint_state_recived==false)
+			{
+				State_handle(i)=msg.position[i];
+			}
+			State(i)=msg.position[i];
 		}
 		if (Joint_state_recived==false)
 		{
@@ -56,10 +62,11 @@ int main(int argc, char **argv) {
 
 
 	State_handle.resize(14);
+	State.resize(14);
 	desiredConfiguration.positions.resize(JOINT_NUMBER);  //5 arm joints + 1 gripper joint
 	armCommand.joint_names.resize(JOINT_NUMBER);
 
-	ros::Rate rate(100); //Hz
+	ros::Rate rate(25); //Hz
 
 
 	while  (Joint_state_recived==false)
@@ -77,11 +84,13 @@ int main(int argc, char **argv) {
 	{
 		ros::spinOnce();
 		cout<<"-------"<<endl;
-		cout<<"State of the robot ";
+		cout<<"ERROR OF State of the robot ";
+		cout<<" "<<(State_handle-State).norm()<<" ";
 		for (int i=0;i<JOINT_NUMBER;i++)
 		{
-			cout<<" "<<State_handle(i)<<" ";
-			desiredConfiguration.positions[i] = State_handle[i]-0.01;
+			desiredConfiguration.positions[i] =(joint_positions_down[i]- State_handle(i))/100+ State_handle(i);
+			State_handle(i)=desiredConfiguration.positions[i];
+		//	desiredConfiguration.positions[i] = joint_positions_down[i];
 		}
 		cout<<"-------"<<endl;
 		armCommand.header.stamp = ros::Time::now();
